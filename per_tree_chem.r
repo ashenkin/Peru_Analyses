d54563a2cb4b52333ffadbd3e881edf87e6334f1
ashenkin <al@shenkin.org>
2015-12-08 09:38:09 +0000

    add non-chambasa trees to chem dataframe

--- per_tree_chem.r
+++ per_tree_chem.r
@@ -8,3 +8,7 @@ library(plyr)
 per_tree_chem = ddply(photosyn, .(tree_id), summarize, mean_c_percent = mean(c_percent, na.rm = T),
                                                        mean_n_percent = mean(n_percent, na.rm = T),
                                                        dbh = dbh[1])
+
+all_trees = get_trees(con)
+all_fp_trees = subset(all_trees, ! grepl("^I", all_trees$tree_code))
+all_trees_with_chem = join(all_fp_trees[,c("tree_id","plot_code","tree_code","fp_species_name","dbh")], per_tree_chem, by = "tree_id", type = "left")
